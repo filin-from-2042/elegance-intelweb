ca8898d84b805e2202873c6e939f24fb89bebdb1
filin-from-2042 <filin2042@gmail.com>
2014-12-06 16:18:31 +0300

    Добавлен модуль "Процесс создания"

--- modules/mod_elegance_our_process/mod_elegance_our_process.php
+++ modules/mod_elegance_our_process/mod_elegance_our_process.php
@@ -11,7 +11,18 @@ $cBase = JURI::base();
 $oDocument 	= JFactory::getDocument();
 $cHeader = $params->get('header');
 $cSubHeader = $params->get('sub_header');
-$cContent = $params->get('text');
+
+$cConceptHeader = $params->get('concept_header');
+$cConceptText = $params->get('concept_text');
+
+$cSpecificationHeader = $params->get('specification_header');
+$cSpecificationText = $params->get('specification_text');
+
+$cLayoutHeader = $params->get('layout_header');
+$cLayoutText = $params->get('layout_text');
+
+$cProgrammingHeader = $params->get('programming_header');
+$cProgrammingText = $params->get('programming_text');
 
 
 require JModuleHelper::getLayoutPath('mod_elegance_our_process', $params->get('layout', 'default'));
--- modules/mod_elegance_our_process/tmpl/default.php
+++ modules/mod_elegance_our_process/tmpl/default.php
@@ -7,43 +7,39 @@
         <div class="w-container">
             <div class="wrap">
                 <div class="process">
-                    <h1 class="our-process-heading">OUR PROCESS</h1>
+                    <h1 class="our-process-heading"><?=$cHeader?></h1>
                     <div class="sepreater"></div>
                 </div>
                 <div class="process-text">
-                    <div class="process-text">Our process is straight forward, simple, &amp; successful.</div>
+                    <div class="process-text"><?=$cSubHeader?></div>
                     <div class="w-row">
                         <div class="w-col w-col-3">
                             <div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
                                 <a href="#set-1" class="hi-icon hi-icon-locked">Security</a>
                             </div>
-                            <h4 class="our-process-sys">BRAINSTORM</h4>
-                            <p class="process-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo
-                                cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</p>
+                            <h4 class="our-process-sys"><?=$cConceptHeader?></h4>
+                            <p class="process-paragraph"><?=$cConceptText?></p>
                         </div>
                         <div class="w-col w-col-3">
                             <div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
                                 <a href="#set-1" class="hi-icon hi-icon-mobile">Mobile</a>
                             </div>
-                            <h4 class="our-process-sys">PLAN</h4>
-                            <p class="process-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo
-                                cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</p>
+                            <h4 class="our-process-sys"><?=$cSpecificationHeader?></h4>
+                            <p class="process-paragraph"><?=$cSpecificationText?></p>
                         </div>
                         <div class="w-col w-col-3">
                             <div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
                                 <a href="#set-1" class="hi-icon hi-icon-screen">Desktop</a>
                             </div>
-                            <h4 class="our-process-sys">DESIGN</h4>
-                            <p class="process-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo
-                                cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</p>
+                            <h4 class="our-process-sys"><?=$cLayoutHeader?></h4>
+                            <p class="process-paragraph"><?=$cLayoutText?></p>
                         </div>
                         <div class="w-col w-col-3">
                             <div class="hi-icon-wrap hi-icon-effect-5 hi-icon-effect-5a">
                                 <a href="#set-1" class="hi-icon hi-icon-earth">Partners</a>
                             </div>
-                            <h4 class="our-process-sys">DEVELOP</h4>
-                            <p class="process-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat. Aenean faucibus nibh et justo
-                                cursus id rutrum lorem imperdiet. Nunc ut sem vitae risus tristique posuere.</p>
+                            <h4 class="our-process-sys"><?=$cProgrammingHeader?></h4>
+                            <p class="process-paragraph"><?=$cProgrammingText?></p>
                         </div>
                     </div>
                 </div>
