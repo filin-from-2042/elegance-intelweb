59fd298eda68ad54d79c42685e24c61f15fb8c50
filin-from-2042 <filin2042@gmail.com>
2014-12-06 17:52:08 +0300

    Добавлен модуль "Команда"

--- modules/mod_elegance_team/mod_elegance_team.php
+++ modules/mod_elegance_team/mod_elegance_team.php
@@ -11,7 +11,17 @@ $cBase = JURI::base();
 $oDocument 	= JFactory::getDocument();
 $cHeader = $params->get('header');
 $cSubHeader = $params->get('sub_header');
-$cContent = $params->get('text');
 
+$cPhotoFirst = $params->get('photo_first');
+$cNameFirst = $params->get('name_first');
+$cDescFirst = $params->get('desc_first');
+
+$cPhotoSecond = $params->get('photo_second');
+$cNameSecond = $params->get('name_second');
+$cDescSecond = $params->get('desc_second');
+
+$cPhotoThird = $params->get('photo_third');
+$cNameThird = $params->get('name_third');
+$cDescThird = $params->get('desc_third');
 
 require JModuleHelper::getLayoutPath('mod_elegance_team', $params->get('layout', 'default'));
--- modules/mod_elegance_team/tmpl/default.php
+++ modules/mod_elegance_team/tmpl/default.php
@@ -7,72 +7,72 @@
         <div class="w-container">
             <div class="wrap">
                 <div class="team">
-                    <h1 class="team">TEAM</h1>
+                    <h1 class="team"><?=$cHeader?></h1>
                     <div class="sepreater team-sep"></div>
                 </div>
                 <div class="team-text">
-                    <p class="team-text-para">We are Elegance. We offer creative solutions for every web-based project you can think of.&nbsp;
-                        <br>We take pride in our work and everything we create is executed with precision and love.</p>
+                    <p class="team-text-para"><?=$cSubHeader?></p>
                 </div>
                 <div class="w-row team-member">
                     <div class="w-col w-col-4">
                         <div class="team-section">
                             <div class="team-image">
-                                <img class="team-img" src="<?=$cBase?>/templates/<?=$oDocument->template?>/images/team/image1.png" alt="52dd1e57e64ce6cb200004f3_image1.png">
+<!--                                <img class="team-img" src="--><?//=$cBase?><!--/templates/--><?//=$oDocument->template?><!--/images/team/image1.png" alt="52dd1e57e64ce6cb200004f3_image1.png">-->
+                                <img class="team-img" src="<?=$cPhotoFirst?>" alt="52dd1e57e64ce6cb200004f3_image1.png">
                             </div>
                             <div class="team-des">
-                                <h4 class="team-name">JANE ANISTON</h4>
-                                <div class="team-name-des">Creative Director</div>
+                                <h4 class="team-name"><?=$cNameFirst?></h4>
+                                <div class="team-name-des"><?=$cDescFirst?></div>
                             </div>
-                            <div class="team-social">
-                                <div class="w-clearfix social-section">
-                                    <a href="https://www.facebook.com/"><img class="social" src="<?=$cBase?>/templates/<?=$oDocument->template?>/images/social/Facebook.png" alt="52dd249c929b601f5400054c_Facebook.png"></a>
-                                    <a href="https://www.twitter.com/"><img class="social" src="<?=$cBase?>/templates/<?=$oDocument->template?>/images/social/Twitter.png" alt="52dd24f2929b601f54000551_Twitter.png"></a>
-                                    <a href="https://www.linkedin.com/"><img class="social" src="<?=$cBase?>/templates/<?=$oDocument->template?>/images/social/Linkedin.png" alt="52dd2672e64ce6cb2000053a_Linkedin.png"></a>
-                                    <a href="https://plus.google.com/"><img class="social" src="<?=$cBase?>/templates/<?=$oDocument->template?>/images/social/Google-plus.png" alt="52dd26a55b54031d540005af_Google-plus.png"></a>
-                                    <a href="https://www.rss.com/"><img class="social" src="<?=$cBase?>/templates/<?=$oDocument->template?>/images/social/RSS.png" alt="52dd26d25b54031d540005b4_RSS.png"></a>
-                                </div>
-                            </div>
+<!--                            <div class="team-social">-->
+<!--                                <div class="w-clearfix social-section">-->
+<!--                                    <a href="https://www.facebook.com/"><img class="social" src="--><?//=$cBase?><!--/templates/--><?//=$oDocument->template?><!--/images/social/Facebook.png" alt="52dd249c929b601f5400054c_Facebook.png"></a>-->
+<!--                                    <a href="https://www.twitter.com/"><img class="social" src="--><?//=$cBase?><!--/templates/--><?//=$oDocument->template?><!--/images/social/Twitter.png" alt="52dd24f2929b601f54000551_Twitter.png"></a>-->
+<!--                                    <a href="https://www.linkedin.com/"><img class="social" src="--><?//=$cBase?><!--/templates/--><?//=$oDocument->template?><!--/images/social/Linkedin.png" alt="52dd2672e64ce6cb2000053a_Linkedin.png"></a>-->
+<!--                                    <a href="https://plus.google.com/"><img class="social" src="--><?//=$cBase?><!--/templates/--><?//=$oDocument->template?><!--/images/social/Google-plus.png" alt="52dd26a55b54031d540005af_Google-plus.png"></a>-->
+<!--                                    <a href="https://www.rss.com/"><img class="social" src="--><?//=$cBase?><!--/templates/--><?//=$oDocument->template?><!--/images/social/RSS.png" alt="52dd26d25b54031d540005b4_RSS.png"></a>-->
+<!--                                </div>-->
+<!--                            </div>-->
                         </div>
                     </div>
                     <div class="w-col w-col-4">
                         <div class="team-section">
                             <div class="team-image">
-                                <img class="team-img" src="<?=$cBase?>/templates/<?=$oDocument->template?>/images/team/image2.png" alt="52dd1edfe64ce6cb200004fb_image2.png">
+                                <img class="team-img" src="<?=$cPhotoSecond?>" alt="52dd1edfe64ce6cb200004fb_image2.png">
                             </div>
                             <div class="team-des">
-                                <h4 class="team-name">JOHN DOE</h4>
-                                <div class="team-name-des">Lead Developer</div>
+                                <h4 class="team-name"><?=$cNameSecond?></h4>
+                                <div class="team-name-des"><?=$cDescSecond?></div>
                             </div>
-                            <div class="team-social">
-                                <div class="w-clearfix social-section">
-                                    <a href="https://www.facebook.com/"><img class="social" src="<?=$cBase?>/templates/<?=$oDocument->template?>/images/social/Facebook.png" alt="52dd249c929b601f5400054c_Facebook.png"></a>
-                                    <a href="https://www.twitter.com/"><img class="social" src="<?=$cBase?>/templates/<?=$oDocument->template?>/images/social/Twitter.png" alt="52dd24f2929b601f54000551_Twitter.png"></a>
-                                    <a href="https://www.linkedin.com/"><img class="social" src="<?=$cBase?>/templates/<?=$oDocument->template?>/images/social/Linkedin.png" alt="52dd2672e64ce6cb2000053a_Linkedin.png"></a>
-                                    <a href="https://plus.google.com/"><img class="social" src="<?=$cBase?>/templates/<?=$oDocument->template?>/images/social/Google-plus.png" alt="52dd26a55b54031d540005af_Google-plus.png"></a>
-                                    <a href="https://www.rss.com/"><img class="social" src="<?=$cBase?>/templates/<?=$oDocument->template?>/images/social/RSS.png" alt="52dd26d25b54031d540005b4_RSS.png"></a>
-                                </div>
-                            </div>
+<!--                            <div class="team-social">-->
+<!--                                <div class="w-clearfix social-section">-->
+<!--                                    <a href="https://www.facebook.com/"><img class="social" src="--><?//=$cBase?><!--/templates/--><?//=$oDocument->template?><!--/images/social/Facebook.png" alt="52dd249c929b601f5400054c_Facebook.png"></a>-->
+<!--                                    <a href="https://www.twitter.com/"><img class="social" src="--><?//=$cBase?><!--/templates/--><?//=$oDocument->template?><!--/images/social/Twitter.png" alt="52dd24f2929b601f54000551_Twitter.png"></a>-->
+<!--                                    <a href="https://www.linkedin.com/"><img class="social" src="--><?//=$cBase?><!--/templates/--><?//=$oDocument->template?><!--/images/social/Linkedin.png" alt="52dd2672e64ce6cb2000053a_Linkedin.png"></a>-->
+<!--                                    <a href="https://plus.google.com/"><img class="social" src="--><?//=$cBase?><!--/templates/--><?//=$oDocument->template?><!--/images/social/Google-plus.png" alt="52dd26a55b54031d540005af_Google-plus.png"></a>-->
+<!--                                    <a href="https://www.rss.com/"><img class="social" src="--><?//=$cBase?><!--/templates/--><?//=$oDocument->template?><!--/images/social/RSS.png" alt="52dd26d25b54031d540005b4_RSS.png"></a>-->
+<!--                                </div>-->
+<!--                            </div>-->
                         </div>
                     </div>
                     <div class="w-col w-col-4">
                         <div class="team-section">
                             <div class="team-image">
-                                <img class="team-img" src="<?=$cBase?>/templates/<?=$oDocument->template?>/images/team/image3.png" alt="52dd1efcad165eca20000539_image3.png">
+                                <img class="team-img" src="<?=$cPhotoThird?>" alt="52dd1efcad165eca20000539_image3.png">
                             </div>
                             <div class="team-des">
-                                <h4 class="team-name">TANE NISTON</h4>
-                                <div class="team-name-des">Graphic Designer</div>
+                                <h4 class="team-name"><?=$cNameThird?></h4>
+                                <div class="team-name-des"><?=$cDescThird?></div>
                             </div>
-                            <div class="team-social">
-                                <div class="w-clearfix social-section">
-                                    <a href="https://www.facebook.com/"><img class="social" src="<?=$cBase?>/templates/<?=$oDocument->template?>/images/social/Facebook.png" alt="52dd249c929b601f5400054c_Facebook.png"></a>
-                                    <a href="https://www.twitter.com/"><img class="social" src="<?=$cBase?>/templates/<?=$oDocument->template?>/images/social/Twitter.png" alt="52dd24f2929b601f54000551_Twitter.png"></a>
-                                    <a href="https://www.linkedin.com/"><img class="social" src="<?=$cBase?>/templates/<?=$oDocument->template?>/images/social/Linkedin.png" alt="52dd2672e64ce6cb2000053a_Linkedin.png"></a>
-                                    <a href="https://plus.google.com/"><img class="social" src="<?=$cBase?>/templates/<?=$oDocument->template?>/images/social/Google-plus.png" alt="52dd26a55b54031d540005af_Google-plus.png"></a>
-                                    <a href="https://www.rss.com/"><img class="social" src="<?=$cBase?>/templates/<?=$oDocument->template?>/images/social/RSS.png" alt="52dd26d25b54031d540005b4_RSS.png"></a>
-                                </div>
-                            </div>
+<!--                            <div class="team-social">-->
+<!--                                <div class="w-clearfix social-section">-->
+<!--                                    <a href="https://www.facebook.com/"><img class="social" src="--><?//=$cBase?><!--/templates/--><?//=$oDocument->template?><!--/images/social/Facebook.png" alt="52dd249c929b601f5400054c_Facebook.png"></a>-->
+<!--                                    <a href="https://www.twitter.com/"><img class="social" src="--><?//=$cBase?><!--/templates/--><?//=$oDocument->template?><!--/images/social/Twitter.png" alt="52dd24f2929b601f54000551_Twitter.png"></a>-->
+<!--                                    <a href="https://www.linkedin.com/"><img class="social" src="--><?//=$cBase?><!--/templates/--><?//=$oDocument->template?><!--/images/social/Linkedin.png" alt="52dd2672e64ce6cb2000053a_Linkedin.png"></a>-->
+<!--                                    <a href="https://plus.google.com/"><img class="social" src="--><?//=$cBase?><!--/templates/--><?//=$oDocument->template?><!--/images/social/Google-plus.png" alt="52dd26a55b54031d540005af_Google-plus.png"></a>-->
+<!--                                    <a href="https://www.rss.com/"><img class="social" src="--><?//=$cBase?><!--/templates/--><?//=$oDocument->template?><!--/images/social/RSS.png" alt="52dd26d25b54031d540005b4_RSS.png"></a>-->
+<!--                                </div>-->
+<!--                            </div>-->
                         </div>
                     </div>
                 </div>
